c8b2193e94bddc3c986a15365190575e2c501cc9
William Showalter <williamshowalter@gmail.com>
2020-05-28 12:20:46 -0400

    Removed template readmes, temp files, and fixed icon path for PluginPackage class.

--- from_cseagle/RevsyncFrontend.java
+++
@@ -1,7 +0,0 @@
-
-import java.util.*;
-
-public interface RevsyncFrontend {
-   public void onmsg_safe(String key, TreeMap<String,Object> data, boolean replay);
-   public void onmsg_safe(String key, TreeMap<String,Object> data);
-}
--- src/main/java/revsyncghidra/RevsyncPluginPackage.java
+++ src/main/java/revsyncghidra/RevsyncPluginPackage.java
@@ -6,11 +6,11 @@ import resources.ResourceManager;
 
 // This class lets us show up in Configure Tool as a main-level category
 public class RevsyncPluginPackage extends PluginPackage {
-	public static final String NAME = "revsync";
+	public static final String NAME = "Revsync";
 	
 	public RevsyncPluginPackage() {
-		super(NAME, ResourceManager.loadImage("images/tango_publicdomain_update.png"),
-				"Revsync Plugin Package Class", EXAMPLES_PRIORITY);
+		super(NAME, ResourceManager.loadImage("images/update_tango_publicdomain.png"),
+				"Revsync plugin", EXAMPLES_PRIORITY);
 		Msg.info(this, "RevSyncPluginPackage constructor");
 	}
 }
